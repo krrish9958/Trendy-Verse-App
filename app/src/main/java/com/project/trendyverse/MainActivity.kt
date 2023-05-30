7e8c4cbb271a93165c519474648c1ed5660fc455
Krrish Kamal <aryakrrish02@gmail.com>
2023-05-30 23:05:12 +0530

    Google sign in option also added

--- app/src/main/java/com/project/trendyverse/MainActivity.kt
+++ app/src/main/java/com/project/trendyverse/MainActivity.kt
@@ -4,7 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.project.trendyverse.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -26,6 +30,22 @@ class MainActivity : AppCompatActivity() {
         binding.loginBtn.setOnClickListener {
             loginUser()
         }
+
+        //googleLogin
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.client_id))
+            .requestEmail()
+            .build()
+        val googleSignInClient = GoogleSignIn.getClient(this, gso)
+        binding.gBtn.setOnClickListener {
+//          googleSignInClient.signOut()
+            startActivityForResult(googleSignInClient.signInIntent,13)
+        }
+
+
+
+
+
 
         binding.forgetPasswordTv.setOnClickListener{
             val intent = Intent(this , ForgetPassword::class.java)
@@ -52,5 +72,28 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 13 && resultCode == RESULT_OK) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            val account = task.getResult(ApiException::class.java)!!
+            firebaseAuthWithGoogle(account.idToken!!)
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        this.auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "Logged in successfully",Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this, HomePageActivity::class.java))
+                    finish()
+                }
+            }.addOnFailureListener {
+                Toast.makeText(this, it.localizedMessage, Toast.LENGTH_LONG).show()
+            }
     }
 }
--- app/src/main/java/com/project/trendyverse/SignUpActivity.kt
+++ app/src/main/java/com/project/trendyverse/SignUpActivity.kt
@@ -4,7 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.project.trendyverse.databinding.ActivityMainBinding
 import com.project.trendyverse.databinding.ActivitySignUpBinding
 
@@ -21,6 +25,19 @@ class SignUpActivity : AppCompatActivity() {
         binding.signUpBtn.setOnClickListener {
             signInUser()
         }
+
+        //googleLogin
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.client_id))
+            .requestEmail()
+            .build()
+        val googleSignInClient = GoogleSignIn.getClient(this, gso)
+        binding.gBtnSignup.setOnClickListener {
+//          googleSignInClient.signOut()
+            startActivityForResult(googleSignInClient.signInIntent,13)
+        }
+
+
 
     }
 
@@ -49,5 +66,30 @@ class SignUpActivity : AppCompatActivity() {
             }
 
         }
+    }
+
+
+    //googleSign in methods
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 13 && resultCode == RESULT_OK) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            val account = task.getResult(ApiException::class.java)!!
+            firebaseAuthWithGoogle(account.idToken!!)
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        this.auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "Logged in successfully",Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this, HomePageActivity::class.java))
+                    finish()
+                }
+            }.addOnFailureListener {
+                Toast.makeText(this, it.localizedMessage, Toast.LENGTH_LONG).show()
+            }
     }
 }
